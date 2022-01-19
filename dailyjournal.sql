ded7e361cc14967787c5221a6cbfb208d2a52a5e
Ashley Dickey <adickey93@gmail.com>
2022-01-19 12:28:01 -0600

    fetc hing

---
+++ dailyjournal.sql
@@ -0,0 +1,39 @@
+CREATE TABLE `Entry` (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`concept`	TEXT NOT NULL,
+	`entry`	TEXT NOT NULL,
+    `date` TEXT NOT NULL,
+    `mood_id` INTEGER NOT NULL,
+    FOREIGN KEY(`mood_id`) REFERENCES `Mood`(`id`)
+);
+
+CREATE TABLE `Mood` (
+    `id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `label`    TEXT NOT NULL
+);
+
+CREATE TABLE `Tag` (
+	`id`  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`tag`  TEXT NOT NULL
+);
+
+CREATE TABLE `Entry_tag` (
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `tag_id` INTEGER NOT NULL,
+    `entry_id` INTEGER NOT NULL,
+    FOREIGN KEY(`tag_id`) REFERENCES `Tag`(`id`),
+    FOREIGN KEY(`entry_id`) REFERENCES `Entry`(`id`)
+);
+
+INSERT INTO `Entry` VALUES (null, 'Python', 'We started python and SQL today', '1/9/22', 1);
+
+INSERT INTO `Mood` VALUES (null, 'excited');
+
+INSERT INTO `Tag` VALUES (null, "python");
+
+INSERT INTO `Entry_tag` VALUES (null, 1, 1);
+
+
+
+
+
---
+++ views/entry_requests.py
@@ -0,0 +1,72 @@
+import sqlite3
+import json
+from models import Entry
+
+def get_all_entries():
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            e.id,
+            e.concept,
+            e.entry,
+            e.date,
+            e.mood_id
+        FROM entry e
+        """)
+
+        entries = []
+
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+
+            # Create an animal instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # Animal class above.
+            entry = Entry(row['id'], row['concept'], row['entry'], row['date'], row['mood_id'])
+
+            entries.append(entry.__dict__)
+
+    # Use `json` package to properly serialize list as JSON
+    return json.dumps(entries)
+
+def get_single_entry(id):
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Use a ? parameter to inject a variable's value
+        # into the SQL statement.
+        db_cursor.execute("""
+        SELECT
+            e.id,
+            e.concept,
+            e.entry,
+            e.date,
+            e.mood_id
+        FROM entry e
+        WHERE e.id = ?
+        """, ( id, ))
+
+        # Load the single result into memory
+        data = db_cursor.fetchone()
+
+        # Create an animal instance from the current row
+        entry = Entry(data['id'], data['concept'], data['entry'],
+                            data['date'], data['mood_id'])
+
+    return json.dumps(entry.__dict__)
+    
+def delete_entry(id):
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        DELETE FROM entry
+        WHERE id = ?
+        """, (id, ))
